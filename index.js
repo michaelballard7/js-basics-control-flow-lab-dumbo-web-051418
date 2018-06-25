3e95f146d86424530642838955754a197315746c
Michael Ballard <ballard.michael9@gmail.com>
2018-06-25 23:11:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ function scuberGreetingForFeet(num){
   if (num <= 400){
     return "This one is on me!"
   } else if(400<num  && num < 2500) {
-      return 'I will gladly take your thirty bucks.'    
+    return 'I will gladly take your thirty bucks.'    
   }else{
     return 'No can do.'
   }
 
+}
+
+
+function ternaryCheckCity(city){
+  
 }
